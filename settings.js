76487ab112355801ba95d47e2ef17e7d6db48245
Tony Blank <me@tonyblank.com>
2016-08-11 15:41:08 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-exports.url = 'http://https://sendgrid-open-source-widget.herokuapp.com/';
+exports.url = 'https://sendgrid-open-source-widget.herokuapp.com/';
 exports.senderEmail = "hello@tonyblank.com";
 exports.senderName = "Tony Blank";
 exports.listID = 492898;
